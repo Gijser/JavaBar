5f25ac0e8b8793cad6a69b2e09d85436122dbdf4
job mekkelholt <job.mekkelholt@student.graafschapcollege.nl>
2021-04-08 11:10:54 +0200

    bruh

--- JavaBarOpdracht/src/javabaropdracht/BarmanModel.java
+++ JavaBarOpdracht/src/javabaropdracht/BarmanModel.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javabaropdracht;
 
-/**
- *
- * @author job26
- */
+
 public class BarmanModel {
-    
+    int bestellingId;
+    int tafel;
+    boolean isKlaar;
+
+    public int getBestellingId() {
+        return bestellingId;
+    }
+
+    public void setBestellingId(int bestellingId) {
+        this.bestellingId = bestellingId;
+    }
+
+    public int getTafel() {
+        return tafel;
+    }
+
+    public void setTafel(int tafel) {
+        this.tafel = tafel;
+    }
+
+    public boolean isIsKlaar() {
+        return isKlaar;
+    }
+
+    public void setIsKlaar(boolean isKlaar) {
+        this.isKlaar = isKlaar;
+    }
 }
--- JavaBarOpdracht/src/javabaropdracht/BarmanView.java
+++ JavaBarOpdracht/src/javabaropdracht/BarmanView.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javabaropdracht;
 
-/**
- *
- * @author job26
- */
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+import javafx.scene.control.CheckBox;
+
 public class BarmanView {
     
+    public Button send;
+    public Label tafel;
+    public CheckBox box;
+    
+    public BarmanView(GridPane p){
+        send = new Button("send");
+        send.setOnAction(event -> send());
+        
+        tafel = new Label("");
+        box = new CheckBox();
+        
+        p.setVgap(5);
+        p.setHgap(8);
+        p.setPadding(new Insets(10, 15, 10, 15));
+        
+        p.add(send, 0, 1);
+        p.add(tafel, 0, 0);
+        p.add(box, 1, 0);
+    }
+    
+    public void send(){
+        tafel.getText();
+        
+        TafelModel tafel = new TafelModel();
+        DrankjeModel drankje = new DrankjeModel();
+    }
 }
